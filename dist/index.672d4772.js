e4f45f50d9f30f768a010ea5b6ba140985db3be9
Titter00 <olo_wojtas@o2.pl>
2022-12-05 17:00:11 +0100

    second commit

---
+++ dist/index.672d4772.js
@@ -0,0 +1,13 @@
+const API_LINK = "https://api.adviceslip.com/advice";
+const adviceNumber = document.querySelector(".advice-generator__advice-number");
+const adviceText = document.querySelector(".advice-generator__quote");
+const generateBtn = document.querySelector(".advice-generator__btn");
+const getAdvice = async ()=>{
+    const response = await fetch(API_LINK);
+    const data = await response.json();
+    adviceNumber.textContent = `Advice #${data.slip.id}`;
+    adviceText.textContent = `Advice #${data.slip.advice}`;
+};
+generateBtn.addEventListener("click", getAdvice);
+
+//# sourceMappingURL=index.672d4772.js.map
